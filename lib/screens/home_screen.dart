ef4c9fe540e51ac1286d74ca24dadaaed2dc371f
jmffelisberto <jm.ffelisberto@gmail.com>
2024-03-01 12:14:30 +0000

    Fetching Google Account Data Successfully

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,25 +5,86 @@ import 'package:multilogin2/screens/login_screen.dart';
 import 'package:multilogin2/utils/next_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen ({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future getData() async {
+    final sp = context.read<SignInProvider>();
+    sp.getDataFromSharedPreferences();
+  }
+
+  @override
+  void initState() {  //for fetching user data purposes
+    super.initState();
+    getData();        //here
+  }
+
   @override
   Widget build(BuildContext context) {
-    final signInProvider = context.read<SignInProvider>();
+    // change read to watch!!!!
+    final sp = context.watch<SignInProvider>();
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          child: Text("Sign Out"),
-          onPressed: () {
-            signInProvider.userSignOut();
-            nextScreenReplace(context, const LoginScreen());
-        }
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              backgroundImage: NetworkImage("${sp.imageUrl}"),
+              radius: 50,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "${sp.name}",
+              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "${sp.email}",
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("PROVIDER:"),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text("${sp.provider}".toUpperCase(),
+                    style: const TextStyle(color: Colors.red)),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black12, // Change the color here
+                ),
+                onPressed: () {
+                  sp.userSignOut();
+                  nextScreenReplace(context, const LoginScreen());
+                },
+                child: const Text("Sign Out",
+                    style: TextStyle(
+                      color: Colors.white70,
+                    )))
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
