3ba3cdf51f38a98f33cb67ef7ea80d8a9a18d288
jmffelisberto <jm.ffelisberto@gmail.com>
2024-03-08 11:00:27 +0000

    More graphic changes & documentation

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# multilogin2
+# MultiLogin
 
-Another trial for various login methods.
+Another trial for skill development purposes.
 
-## Getting Started
+## Features
 
-This project is a starting point for a Flutter application.
+### Authentication Methods
 
-A few resources to get you started if this is your first Flutter project:
+- Google Account Login
+- Facebook Account Login
+- Phone Number w/ SMS confirmation
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+### Storage
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- *Coming Soon...*
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:multilogin2/provider/sign_in_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:multilogin2/screens/login_screen.dart';
@@ -50,21 +51,33 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Text(
               "${sp.email}",
-              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
             ),
             const SizedBox(
-              height: 10,
+              height: 15,
             ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("PROVIDER:"),
                 const SizedBox(
                   width: 5,
                 ),
-                Text("${sp.provider}".toUpperCase(),
-                    style: const TextStyle(color: Colors.red)),
+                if (sp.provider == "FACEBOOK")
+                  Icon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.blue,
+                  ),
+                if (sp.provider == "GOOGLE")
+                  Icon(
+                    FontAwesomeIcons.google,
+                    color: Colors.red,
+                  ),
+                if (sp.provider == "PHONE")
+                  Icon(
+                    FontAwesomeIcons.phone,
+                    color: Colors.green,
+                  ),
               ],
             ),
             const SizedBox(
